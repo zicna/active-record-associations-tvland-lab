42a9a99810f0398ee1902bb2c289c1daacc43ed7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-03-01 18:31:01 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :actors, through: :characters
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map do |char|
+        #binding.pry
+        "#{char.actor.first_name} #{char.actor.last_name}"
+    end
+  end
+
 end
